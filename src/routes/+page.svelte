24c6ab670244a05244f21477d7bbbb36f47f7341
Noah <mounderfod@gmail.com>
2022-10-22 17:03:22 +0100

    feat(frontend): :lipstick: Rearrange home page tiles into horizontal grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
    import { t } from '$lib/translations' 
-   import { ClickableTile } from 'carbon-components-svelte';
+   import { ClickableTile, Column, Grid, Row } from 'carbon-components-svelte';
    import Play from "carbon-icons-svelte/lib/Play.svelte";
    import WatsonHealth3DSoftware from "carbon-icons-svelte/lib/WatsonHealth3DSoftware.svelte";
    import Cube from "carbon-icons-svelte/lib/Cube.svelte";
@@ -8,25 +8,35 @@
 </script>
 
 <h1 style="margin-bottom: 1rem;">{$t('home.welcome')}</h1>
-<ClickableTile>
-   <h4><Play/> Quick Launch</h4>
-   <p>Launch last played instance: {lastInstance}</p>
-</ClickableTile>
-<ClickableTile>
-   <h4><WatsonHealth3DSoftware/> New Instance</h4>
-   <p>Create a new instance</p>
-</ClickableTile>
-<ClickableTile style="margin-bottom: 1rem;">
-   <h4><Cube/> View Instances</h4>
-   <p>Manage your installed instances</p>
-</ClickableTile>
+<Grid fullWidth noGutter>
+   <Row>
+      <Column>
+         <ClickableTile>
+            <h4><Play/> Quick Launch</h4>
+            <p>Launch last played instance: {lastInstance}</p>
+         </ClickableTile>
+      </Column>
+      <Column>
+         <ClickableTile>
+            <h4><WatsonHealth3DSoftware/> New Instance</h4>
+            <p>Create a new instance</p>
+         </ClickableTile>
+      </Column>
+      <Column>
+         <ClickableTile style="margin-bottom: 1rem;">
+            <h4><Cube/> View Instances</h4>
+            <p>Manage your installed instances</p>
+         </ClickableTile>
+      </Column>
+   </Row>
+</Grid>
 
 <h2 style="margin-bottom: 1rem;">News</h2>
 <ClickableTile>
    <h4>News goes here.</h4>
    <p>What a new news article!</p>
 </ClickableTile>
-<ClickableTile>
+<ClickableTile style="margin-bottom: 1rem;">
    <h4>Even more news</h4>
    <p>Wow!</p>
 </ClickableTile>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ['@carbon/charts', 'carbon-components'],
+	},
 };
 
 export default config;
