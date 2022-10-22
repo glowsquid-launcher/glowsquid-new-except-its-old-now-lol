b6e58948c83053489dc0d08704d5a13cd2cc3dfe
Noah <mounderfod@gmail.com>
2022-10-22 16:33:12 +0100

    feat(frontend): :lipstick: Home page UI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,19 @@
 <script>
     import "carbon-components-svelte/css/g100.css";
-    import { Header, HeaderNav, HeaderNavItem, Content } from 'carbon-components-svelte';
+    import { Header, HeaderNav, HeaderNavItem, Content, HeaderUtilities, HeaderGlobalAction } from 'carbon-components-svelte';
+    import { t } from '$lib/translations'
+    import Settings from 'carbon-icons-svelte/lib/Settings.svelte'
 </script>
 
-<Header company="Glowsquid Launcher" platformName="ALPHA">
+<Header company="{$t('common.title')}" platformName="ALPHA">
     <HeaderNav>
-        <HeaderNavItem href="/" text="Home" icon="home"></HeaderNavItem>
-        <HeaderNavItem text="Instances" icon=""></HeaderNavItem>
-        <HeaderNavItem text="Modpacks"></HeaderNavItem>
+        <HeaderNavItem href="/" text="{$t('common.home')}"></HeaderNavItem>
+        <HeaderNavItem text="{$t('common.instances')}" icon=""></HeaderNavItem>
+        <HeaderNavItem text="{$t('common.modpacks')}"></HeaderNavItem>
     </HeaderNav>
+    <HeaderUtilities>
+        <HeaderGlobalAction aria-label="settings" icon={Settings}></HeaderGlobalAction>
+    </HeaderUtilities>
 </Header>
 
 <Content>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,34 @@
 <script>
    import { t } from '$lib/translations' 
+   import { ClickableTile } from 'carbon-components-svelte';
+   import Play from "carbon-icons-svelte/lib/Play.svelte";
+   import WatsonHealth3DSoftware from "carbon-icons-svelte/lib/WatsonHealth3DSoftware.svelte";
+   import Cube from "carbon-icons-svelte/lib/Cube.svelte";
+   var lastInstance = "1.18.2";
 </script>
 
-<p>{$t('home.welcome')}</p>
+<h1 style="margin-bottom: 1rem;">{$t('home.welcome')}</h1>
+<ClickableTile>
+   <h4><Play/> Quick Launch</h4>
+   <p>Launch last played instance: {lastInstance}</p>
+</ClickableTile>
+<ClickableTile>
+   <h4><WatsonHealth3DSoftware/> New Instance</h4>
+   <p>Create a new instance</p>
+</ClickableTile>
+<ClickableTile style="margin-bottom: 1rem;">
+   <h4><Cube/> View Instances</h4>
+   <p>Manage your installed instances</p>
+</ClickableTile>
+
+<h2 style="margin-bottom: 1rem;">News</h2>
+<ClickableTile>
+   <h4>News goes here.</h4>
+   <p>What a new news article!</p>
+</ClickableTile>
+<ClickableTile>
+   <h4>Even more news</h4>
+   <p>Wow!</p>
+</ClickableTile>
+
+
