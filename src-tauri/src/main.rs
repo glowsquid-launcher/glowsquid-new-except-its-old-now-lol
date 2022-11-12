7e1d73d7aac0fc411c8018e23ee00221d42f2908
Suyashtnt <suyashtnt@gmail.com>
2022-11-12 11:13:56 +0200

    feat(backend): :heavy_plus_sign: add `copper` to glowsquid
    this allows bindings to the copper crate, in order to launch minecraft

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,13 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_copper::init(
+            "2aa32806-92e3-4242-babc-392ac0f0fd30".to_owned(),
+        ))
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
